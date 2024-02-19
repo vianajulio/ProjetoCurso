b83b5ba29fada74ae44a00ba138c11dbcb4335d9
vianajulio <juliogabrielv8@gmail.com>
2024-02-19 09:45:08 -0300

    Exercicio Livros

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ProjetoCurso.Exercicios.Livros;
+
+class Program
+{
+    static async Task Main()
+    {
+        await LivrosHttpClient.LivrosHttp();
+    }
+}
