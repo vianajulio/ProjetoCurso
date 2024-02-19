73999f32c54876e82e92819ee08bbf7cb7185f83
ynextson <85638741+ynextson@users.noreply.github.com>
2024-02-19 09:07:26 -0300

    Adicionando Filmes

---
+++ Exercicios/Filmes/Filme.cs
@@ -0,0 +1,21 @@
+﻿using ProjetoCurso;
+using System.Text.Json;
+internal class Filme
+{
+    private static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string respFilmes = await client.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/TopMovies.json");
+                var filmes = JsonSerializer.Deserialize<List<FilmeDesserializer>>(respFilmes);
+                filmes[4].MostarDetalhesFilme();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro {e.Message}");
+            }
+        }
+    }
+}
