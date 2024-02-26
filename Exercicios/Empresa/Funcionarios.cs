b56a1714c2348b9e1404c6862515c7a49dcb407f
ynextson <85638741+ynextson@users.noreply.github.com>
2024-02-26 11:32:15 -0300

    Adicionado Empresa

---
+++ Exercicios/Empresa/Funcionarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCurso.Exercicios.Empresa
+{
+    class Funcionarios
+    {
+        public string Nome { get; set; }
+        public string Salario { get; set; }
+    }
+}
+
+
+//Crie uma hierarquia de classes representando funcionários de uma empresa. Utilize herança para criar classes como Gerente,
+//    Programador e Analista. Cada classe deve ter propriedades específicas, além das propriedades comuns a todos os funcionários, como Nome e Salário.
